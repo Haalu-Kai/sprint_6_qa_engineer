c9bcd436025fcc07b89c9ad65d81078449791967
Haalu-Kai <tinavie00@gmail.com>
2026-03-30 10:29:54 +0300

    Update base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -9,12 +9,16 @@ class BasePage:
         self.driver = driver
 
     def find_element(self, locator, time=10):
-        return WebDriverWait(self.driver, time).until(EC.presence_of_element_located(locator),
-                                                      message=f"Can't find element by locator {locator}")
+        return WebDriverWait(self.driver, time).until(
+            EC.presence_of_element_located(locator),
+            message=f"Can't find element by locator {locator}"
+        )
 
     def find_elements(self, locator, time=10):
-        return WebDriverWait(self.driver, time).until(EC.presence_of_all_elements_located(locator),
-                                                      message=f"Can't find elements by locator {locator}")
+        return WebDriverWait(self.driver, time).until(
+            EC.presence_of_all_elements_located(locator),
+            message=f"Can't find elements by locator {locator}"
+        )
 
     @allure.step('Перейти по адресу')
     def go_to_site(self, url=None):
@@ -23,5 +27,15 @@ class BasePage:
         self.driver.get(url)
 
     @allure.step('Получить текущий URL')
-    def current_url(self):
+    def get_current_url(self):
         return self.driver.current_url
+
+    @allure.step('Переключиться на вкладку браузера')
+    def switch_to_window(self, window_number: int = 1):
+        self.driver.switch_to.window(self.driver.window_handles[window_number])
+
+    @allure.step('Ожидать, пока URL не станет не about:blank')
+    def wait_url_until_not_about_blank(self, time: int = 10):
+        WebDriverWait(self.driver, time).until_not(
+            EC.url_to_be('about:blank')
+        )
